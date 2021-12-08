2538682cd60c07c6372a00bf83697a9ac47199e5
nguyenphuctinh <phuctinh2001@gmail.com>
2021-12-08 21:54:32 +0700

    make it better

--- server.js
+++ server.js
@@ -3,5 +3,12 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("test deploy nodejs NGuyễn Phsuc Tĩnh");
 });
+app.get("/user", (req, res) => {
+  res.json([
+    { name: "Tĩnh", age: 20 },
+    { name: "Việt Anh", age: 20 },
+    { name: "Huyền Anh", age: 18 },
+  ]);
+});
 const port = process.env.PORT || 3000;
 app.listen(port);
