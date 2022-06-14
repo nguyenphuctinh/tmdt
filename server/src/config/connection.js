576db8de75c15fcc65fc412ea57136d4b5848550
nguyenphuctinh <phuctinh2001@gmail.com>
2022-06-14 11:20:48 +0700

    test

--- server/src/config/connection.js
+++ server/src/config/connection.js
@@ -4,7 +4,14 @@ import mysql from "mysql";
 heroku logs --tail
 git push heroku master
 */
+// var db_config = {
 
+//   host: "us-cdbr-east-05.cleardb.net",
+//   user: "bcfc9b7ab00c04",
+//   port: "3306",
+//   password: "bd6dc0ce",
+//   database: "heroku_fdea9aba84c6f63",
+// };
 var db_config = {
   host: "localhost",
   user: "root",
